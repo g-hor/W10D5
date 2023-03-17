7813590ca0292f2753841550e4741528cc2b26a2
Zane <zeisen33@gmail.com>
2023-03-17 16:52:20 -0400

    Phase 3

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,7 +1,7 @@
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
 import GalleryView from "./components/GalleryView";
-import { Route } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 
 
 function App() {
@@ -9,10 +9,35 @@ function App() {
   return (
     <div className="page-wrapper">
       <GalleryNavigation galleries={harvardArt.records}/>
-      <Route 
-        path="/galleries/:galleryId" 
-        render={() => <GalleryView galleries={harvardArt.records} />} 
-      />
+      <Switch>
+        <Route path='/error' render={() => {
+          return (
+            <h2>
+              Page Not Found
+            </h2>
+          )
+        }} />
+        <Route 
+          path="/galleries/:galleryId" 
+          render={() => <GalleryView galleries={harvardArt.records} />} 
+        />
+        <Route 
+          exact path="/"
+          render={() => {
+            return (
+              <>
+                <h2>
+                  Harvard Art Museum
+                </h2>
+                <p>
+                  Look, but Don't Touch. Please select a Gallery in the navigation bar.
+                </p>
+              </>
+            )
+          }} 
+        />
+        <Redirect to='/error' /> 
+      </Switch>
     </div>
   );
 }
